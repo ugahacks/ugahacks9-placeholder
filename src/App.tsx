3ab5b3cf253cfa2ab673c58c006f6e640d205f0e
Shawn Pradeep <shawnprdp@gmail.com>
2023-06-19 10:26:45 -0400

    init placeholder for uh9

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Logo from "./components/Logo/Logo";
+import mlhBanner from "./images/mlh-trust-badge-2024-white.svg";
+import byteFlying from "./images/byte flying.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img className="mlh-banner" src={mlhBanner}></img>
+      <Logo />
+      <div className="background-image"></div>
     </div>
   );
 }
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Logo.css";
+import uh9Logo from "../../images/ugahackslogo_pinched.svg";
+
+import facebookIcon from "../../images/icons/facebook-icon.svg";
+import instagramIcon from "../../images/icons/instagram-icon.svg";
+import linkedinIcon from "../../images/icons/linkedin-icon.svg";
+import githubIcon from "../../images/icons/github-icon.svg";
+
+function Logo() {
+  return (
+    <div className="flex-container">
+      <div className="row">
+        <img className="logo" src={uh9Logo} alt="ugahacks 9 logo w/ dog"></img>
+      </div>
+      <div className="row">
+        <h1>COMING SOON</h1>
+      </div>
+      <div className="row">
+        <button className="register-button">PRE-REGISTER</button>
+      </div>
+      <div className="row">
+        <div className="social-container">
+          <a
+            href="https://www.instagram.com/ugahacks/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagramIcon}></img>
+          </a>
+          <a
+            href="https://www.linkedin.com/company/ugahacks"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedinIcon}></img>
+          </a>
+          <a
+            href="https://github.com/ugahacks"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubIcon}></img>
+          </a>
+          <a
+            href="https://www.facebook.com/ugahacks/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebookIcon}></img>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
